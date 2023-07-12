3c46d625bdced570437a541aab1c04bde3a87f2e
Lartey Solomon <93642216+GeekYbruCe@users.noreply.github.com>
2023-07-12 19:54:31 +0000

    Update MainActivity.java
    Added Comments

--- app/src/main/java/com/example/myquizapplication/MainActivity.java
+++ app/src/main/java/com/example/myquizapplication/MainActivity.java
@@ -9,22 +9,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+// This class is the main activity of the quiz app.
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView totalQuestionsTextView;
     TextView questionTextView;
     Button ansA,ansB,ansC,ansD;
     Button submitBtn;
 
+    // These variables store the total number of questions, the current question index, and the selected answer.
     int score=0;
     int totalQuestion= QuestionAnswer.question.length;
     int currentQuestionIndex=0;
     String selectedAnswer = "";
 
+    // This method is called when the activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+       // Initialize the UI elements.
         totalQuestionsTextView = findViewById(R.id.total_question);
         questionTextView = findViewById(R.id.question);
         ansA = findViewById(R.id.ans_A);
@@ -33,17 +36,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ansD = findViewById(R.id.ans_D);
         submitBtn = findViewById(R.id.submit_btn);
 
+        // Set the click listeners for the buttons.
         ansA.setOnClickListener(this);
         ansB.setOnClickListener(this);
         ansC.setOnClickListener(this);
         ansD.setOnClickListener(this);
         submitBtn.setOnClickListener(this);
-
+       // Set the total number of questions text view.
         totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+        
+         // Load the first question.
         loadNewQuestion();
 
     }
 
+     // This method is called when a button is clicked.
     @Override
     public void onClick(View view) {
         ansA.setBackgroundColor(Color.WHITE);
@@ -51,8 +58,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ansC.setBackgroundColor(Color.WHITE);
         ansD.setBackgroundColor(Color.WHITE);
 
-
+        // Get the button that was clicked.
         Button clickedButton = (Button) view;
+
+        // If the submit button was clicked, check the answer and load the next question.
         if (clickedButton.getId()==R.id.submit_btn){
             if (selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
                 score++;  }
@@ -61,18 +70,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         }else{
-//            choices button clicked
+          // The answer button was clicked.
             selectedAnswer = clickedButton.getText().toString();
             clickedButton.setBackgroundColor(Color.MAGENTA);
         }
     }
-    void loadNewQuestion(){
 
+    // This method loads the next question.
+    void loadNewQuestion(){
+ 
+        // If the current question index is the total number of questions, show the finish quiz dialog.
         if (currentQuestionIndex==totalQuestion){
             finishQuiz();
             return;
         }
 
+        // Set the question and answer text views.
         questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
         ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
         ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
@@ -80,6 +93,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
 
     }
+    
+    // This method shows the finish quiz dialog.
     void finishQuiz(){
         String passStatus = "";
         if (score> totalQuestion*0.6){
